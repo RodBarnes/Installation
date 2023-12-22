251d059106be6028f38f2a158e707626eec8a889
AdvApp <advappsw@gmail.com>
2023-12-21 17:11:58 -0800

    Additional settings for nemo

--- fedora/config2.sh
+++ fedora/config2.sh
@@ -44,12 +44,14 @@ gsettings set org.cinnamon.desktop.interface clock-use-24h false
 gsettings set org.cinnamon.desktop.media-handling automount-open false
 gsettings set org.cinnamon panels-height "['1:40']"
 gsettings set org.cinnamon panels-autohide "['1:intel']"
+gsettings set org.x.apps.portal color-scheme 'prefer-dark'
 gsettings set org.nemo.preferences show-computer-icon-toolbar true
 gsettings set org.nemo.preferences show-open-in-terminal-toolbar true
 gsettings set org.nemo.desktop computer-icon-visible false
 gsettings set org.nemo.desktop home-icon-visible false
 gsettings set org.nemo.desktop trash-icon-visible false
-gsettings set org.x.apps.portal color-scheme 'prefer-dark'
+gsettings set org.nemo.preferences quick-renames-with-pause-in-between true
+gsettings set org.nemo.preferences show-advanced-permissions true
 
 # Configure editor preferences
 gsettings set org.x.editor.preferences.editor prefer-dark-theme true
