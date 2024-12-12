f0aae343bb8d9bd790313da27e34d5603a9b1a72
AdvApp <advappsw@gmail.com>
2024-12-11 19:12:03 -0700

    Syncing bunch of changes made locally but hadn't been pushed to repository -- my bad.

--- mint/apps_extra.sh
+++ mint/apps_extra.sh
@@ -20,63 +20,7 @@ sudo apt install -y picard
 sudo apt install -y sound-juicer
 sudo apt install -y ffmpeg
 sudo apt install -y pdftk-java
-
-# Add/configure git
-sudo apt install -y git
-git config --global user.name "Rod Barnes"
-git config --global user.email "advappsw@gmail.com"
-
-# Add yt-dlp
-wget -O ~/Downloads/yt-dlp_linux https://github.com/yt-dlp/yt-dlp/releases/download/2023.12.30/yt-dlp_linux
-sudo mv ~/Downloads/yt-dlp_linux /usr/local/bin/yt-dlp
-sudo chgrp root /usr/local/bin/yt-dlp
-sudo chown root /usr/local/bin/yt-dlp
-sudo chmod +x /usr/local/bin/yt-dlp
-
-# Add Meld
-flatpak install -y flathub org.gnome.meld
-
-# Add VSCodium
-wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg \
-    | gpg --dearmor \
-    | sudo dd of=/usr/share/keyrings/vscodium-archive-keyring.gpg
-echo 'deb [ signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg ] https://download.vscodium.com/debs vscodium main' \
-    | sudo tee /etc/apt/sources.list.d/vscodium.list
-sudo apt update && sudo apt install codium
-
-# Add MoneyDance
-wget -O ~/Downloads/moneydance_linux_amd64.deb  https://infinitekind.com/stabledl/current/moneydance_linux_amd64.deb
-sudo apt install -y ~/Downloads/moneydance_linux_amd64.deb
-
-# Add Postman
-wget -O ~/Downloads/postman-linux-x64.tar.gz https://dl.pstmn.io/download/latest/linux_64
-cd /opt
-sudo tar xf ~/Downloads/postman-linux-x64.tar.gz
-sudo ln -s /opt/Postman/Postman /usr/local/bin/postman
-sudo cat <<EOF >/usr/local/share/applications/postman.desktop
-[Desktop Entry]
-Name=Postman
-Exec=postman
-Comment=Web call investigator
-Terminal=false
-Type=Application
-Icon=/opt/Postman/app/icons/icon_128x128.png
-Categories=Development
-EOF
-
-# Add Slack
-flatpak install flathub com.slack.Slack
-#printx "Manually download the slack DEB from this URL 'https://slack.com/downloads/linux'"
-#sudo apt install ~/Downloads/slack-desktop-4.35.131-amd64.deb
-
-# Add Zoom
-wget -O ~/Downloads/zoom_amd64.deb https://zoom.us/client/5.17.1.1840/zoom_amd64.deb
-sudo apt install -y ~/Downloads/zoom_amd64.deb
-
-# Add iDrive
-wget -O ~/Downloads/idriveforlinux.bin https://www.idrivedownloads.com/downloads/linux/download-for-linux/linux-bin/idriveforlinux.bin
-sudo mv /home/rod/Downloads/idriveforlinux.bin /opt
-sudo bash /opt/idriveforlinux.bin --install
-
-# Update to add all menu items
-sudo update-desktop-database
+sudo apt install -y vlc
+sudo apt install -y dconf-editor
+sudo apt install -y nemo-image-converter
+sudo apt install -y nemo-gtkhash
--- mint/apps_remove.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-#v1.0
-
-sudo apt remove -y hypnotix hexchat libreoffice-math cinnamon-screensaver
