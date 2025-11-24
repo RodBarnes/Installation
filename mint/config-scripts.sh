f504282745b47e336b97e85120c47771cca66a74
AdvApp <advappsw@gmail.com>
2025-11-24 15:31:35 -0700

    Renamed underscores to hyphen

--- mint/config-scripts.sh
+++ mint/config-scripts.sh
@@ -2,7 +2,6 @@
 #v1.00
 
 DIR=$(dirname $(readlink -f $0))
-#echo "DIR=${DIR}"
 
 if [[ "$EUID" = 0 ]]; then
   echo "This must be run as the standard user that will use the device.\nIt will prompt for sudo when it is needed."
@@ -11,9 +10,17 @@ fi
 
 # Copy the scripts, make them executable, then move then to /usr/local/bin
 sudo mkdir /tmp/TOOL_SCRIPTS
-sudo cp bin/* /tmp/TOOL_SCRIPTS
+
+sudo cp "$DIR/../scripts/bin/*" /tmp/TOOL_SCRIPTS
+sudo chown root:root /tmp/TOOL_SCRIPTS/*
 sudo chmod +x /tmp/TOOL_SCRIPTS/*
-sudo chgrp root /tmp/TOOL_SCRIPTS/*
-sudo chown root /tmp/TOOL_SCRIPTS/*
-sudo mv /tmp/TOOL_SCRIPTS/* /usr/local/bin
+for file in */tmp/TOOL_SCRIPTS/*; do
+	sudo mv "$file" "/usr/local/bin/${file%.sh}"
+done
+
+sudo cp "$DIR/../scripts/lib/*" /tmp/TOOL_SCRIPTS
+sudo chown root:root /tmp/TOOL_SCRIPTS/*
+sudo mv /tmp/TOOL_SCRIPTS/* /usr/local/lib
+
 sudo rmdir /tmp/TOOL_SCRIPTS
+
