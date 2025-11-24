af9925cd07a14880250569b5b246d88a1226c45d
AdvApp <advappsw@gmail.com>
2025-11-24 15:15:45 -0700

    Remvoed dependence upon colors.sh

--- mint/config_desktop.sh
+++ mint/config_desktop.sh
@@ -4,13 +4,8 @@
 DIR=$(dirname $(readlink -f $0))
 #echo "DIR=${DIR}"
 
-source ${DIR}/colors.sh
-function printx {
-  printf "${YELLOW}$1${NOCOLOR}\n"
-}
-
 if [[ "$EUID" = 0 ]]; then
-  printx "This must be run as the standard user that will use the device.\nIt will prompt for sudo when it is needed.\n"
+  echo "This must be run as the standard user that will use the device.\nIt will prompt for sudo when it is needed."
   exit
 fi
 
